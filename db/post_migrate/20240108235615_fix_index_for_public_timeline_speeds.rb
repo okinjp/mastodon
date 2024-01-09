407b98acfeb8584a3462b616ff2b4b8584efd271
KMY <tt@kmycode.net>
2024-01-09 09:23:02 +0900

    Fix: #432 連合タイムラインのインデックスが正しく動作しない

---
+++ db/post_migrate/20240108235615_fix_index_for_public_timeline_speeds.rb
@@ -0,0 +1,22 @@
+# frozen_string_literal: true
+
+class FixIndexForPublicTimelineSpeeds < ActiveRecord::Migration[7.1]
+  disable_ddl_transaction!
+
+  # rubocop:disable Naming/VariableNumber
+  def up
+    add_index :statuses, [:id], name: :index_statuses_local_20240108, algorithm: :concurrently, order: { id: :desc }, where: '(local OR (uri IS NULL)) AND deleted_at IS NULL AND visibility IN (0, 10, 11) AND reblog_of_id IS NULL AND ((NOT reply) OR (in_reply_to_account_id = account_id))'
+    add_index :statuses, [:id], name: :index_statuses_public_20240108, algorithm: :concurrently, order: { id: :desc }, where: 'deleted_at IS NULL AND visibility IN (0, 10, 11) AND reblog_of_id IS NULL AND ((NOT reply) OR (in_reply_to_account_id = account_id))'
+    remove_index :statuses, name: :index_statuses_local_20231213
+    remove_index :statuses, name: :index_statuses_public_20231213
+  end
+
+  def down
+    add_index :statuses, [:id, :account_id], name: :index_statuses_local_20231213, algorithm: :concurrently, order: { id: :desc }, where: '(local OR (uri IS NULL)) AND deleted_at IS NULL AND visibility IN (0, 10, 11) AND reblog_of_id IS NULL AND ((NOT reply) OR (in_reply_to_account_id = account_id))'
+    add_index :statuses, [:id, :account_id], name: :index_statuses_public_20231213, algorithm: :concurrently, order: { id: :desc }, where: 'deleted_at IS NULL AND visibility IN (0, 10, 11) AND reblog_of_id IS NULL AND ((NOT reply) OR (in_reply_to_account_id = account_id))'
+    remove_index :statuses, name: :index_statuses_local_20240108
+    remove_index :statuses, name: :index_statuses_public_20240108
+  end
+
+  # rubocop:enable Naming/VariableNumber
+end
